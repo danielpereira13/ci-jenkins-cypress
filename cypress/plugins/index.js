ce7c495e7f7c9196f722f1c9eaf41f2853254734
dp735281 <daniel.pereira@broadcom.com>
2020-06-17 23:43:59 +0100

    kk

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,21 @@
-const fs = require('fs-extra');
-const path = require('path');
-const selectTestsWithGrep = require('cypress-select-tests/grep');
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 
 /**
- * Reads Cypress configuration for a specific environment
- * @param {string} file The file name.
- * @return {Object} The parsed Cypress configuration.
+ * @type {Cypress.PluginConfig}
  */
-function getConfigurationByFile(file) {
-  const pathToConfigFile = path.resolve('config', `${file}.json`);
-  return fs.readJson(pathToConfigFile);
-}
-
 module.exports = (on, config) => {
-  // setup select tests plugin
-  on('file:preprocessor', selectTestsWithGrep(config));
-
-  // accept a configFile value or use local by default
-  const file = config.env.configFile || 'local';
-  return getConfigurationByFile(file);
-};
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
