c029948d628840ffdf7c1ab3a61dde16c725c3af
dp735281 <daniel.pereira@broadcom.com>
2020-06-18 15:49:38 +0100

    ff

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -52,7 +52,7 @@ context('Actions', () => {
     cy.get('.action-clear').type('Clear this text')
       .should('have.value', 'Clear this text')
       .clear()
-      .should('have.value', '')
+      .should('have.value', 'XPTO')
   })
 
   it('.submit() - submit a form', () => {
