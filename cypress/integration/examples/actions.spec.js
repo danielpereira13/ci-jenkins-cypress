862381e5152a7859be4ec73302d8a4f290e125b5
dp735281 <daniel.pereira@broadcom.com>
2020-07-09 01:27:20 +0100

    aaaa

--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-context('Actions'+ browser, () => {
+context('Actions', () => {
   beforeEach(() => {
     cy.visit('https://example.cypress.io/commands/actions')
   })
